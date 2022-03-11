4e5267c5549f08241bcf0162ec9aa28b3393c10c
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-03-11 16:52:06 -0300

    l

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI:  "mongodb+srv://ues:711179 @cluster0.z9jbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI:  "mongodb+srv://ues:711179@cluster0.z9jbb.mongodb.net/Amai",
     secretOrKey: "secret"
   };
