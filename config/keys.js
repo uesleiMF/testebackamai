66889f0378c91e2b5c747de05636ddbf7585ca87
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-03-11 16:19:55 -0300

    back

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:  "mongodb+srv://ues:711179 @cluster0.z9jbb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+  };
--- server.js
+++ server.js
@@ -1,8 +1,3 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-
 var express = require("express");
 var app = express();
 const bcrypt = require('bcrypt');
@@ -11,9 +6,11 @@ var cors = require('cors');
 var multer = require('multer'),
   bodyParser = require('body-parser'),
   path = require('path');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/productDB");
 var fs = require('fs');
-var produto = require("./models/produto.js");
-var user = require("./models/user.js");
+var product = require("./model/product.js");
+var user = require("./model/user.js");
 
 var dir = './uploads';
 var upload = multer({
@@ -184,19 +181,19 @@ function checkUserAndGenerateToken(data, req, res) {
 }
 
 /* Api to add Product */
-app.post("/add-produto", upload.any(), (req, res) => {
+app.post("/add-product", upload.any(), (req, res) => {
   try {
     if (req.files && req.body && req.body.name && req.body.desc && req.body.price &&
       req.body.discount) {
 
-      let new_produto = new produto();
-      new_produto.name = req.body.name;
-      new_produto.desc = req.body.desc;
-      new_produto.price = req.body.price;
-      new_produto.image = req.files[0].filename;
-      new_produto.discount = req.body.discount;
-      new_produto.user_id = req.user.id;
-      new_produto.save((err, data) => {
+      let new_product = new product();
+      new_product.name = req.body.name;
+      new_product.desc = req.body.desc;
+      new_product.price = req.body.price;
+      new_product.image = req.files[0].filename;
+      new_product.discount = req.body.discount;
+      new_product.user_id = req.user.id;
+      new_product.save((err, data) => {
         if (err) {
           res.status(400).json({
             errorMessage: err,
@@ -225,36 +222,36 @@ app.post("/add-produto", upload.any(), (req, res) => {
 });
 
 /* Api to update Product */
-app.post("/update-produto", upload.any(), (req, res) => {
+app.post("/update-product", upload.any(), (req, res) => {
   try {
     if (req.files && req.body && req.body.name && req.body.desc && req.body.price &&
       req.body.id && req.body.discount) {
 
-      produto.findById(req.body.id, (err, new_produto) => {
+      product.findById(req.body.id, (err, new_product) => {
 
         // if file already exist than remove it
-        if (req.files && req.files[0] && req.files[0].filename && new_produto.image) {
-          var path = `./uploads/${new_produto.image}`;
+        if (req.files && req.files[0] && req.files[0].filename && new_product.image) {
+          var path = `./uploads/${new_product.image}`;
           fs.unlinkSync(path);
         }
 
         if (req.files && req.files[0] && req.files[0].filename) {
-          new_produto.image = req.files[0].filename;
+          new_product.image = req.files[0].filename;
         }
         if (req.body.name) {
-          new_produto.name = req.body.name;
+          new_product.name = req.body.name;
         }
         if (req.body.desc) {
-          new_produto.desc = req.body.desc;
+          new_product.desc = req.body.desc;
         }
         if (req.body.price) {
-          new_produto.price = req.body.price;
+          new_product.price = req.body.price;
         }
         if (req.body.discount) {
-          new_produto.discount = req.body.discount;
+          new_product.discount = req.body.discount;
         }
 
-        new_produto.save((err, data) => {
+        new_product.save((err, data) => {
           if (err) {
             res.status(400).json({
               errorMessage: err,
@@ -285,10 +282,10 @@ app.post("/update-produto", upload.any(), (req, res) => {
 });
 
 /* Api to delete Product */
-app.post("/delete-produto", (req, res) => {
+app.post("/delete-product", (req, res) => {
   try {
     if (req.body && req.body.id) {
-      produto.findByIdAndUpdate(req.body.id, { is_delete: true }, { new: true }, (err, data) => {
+      product.findByIdAndUpdate(req.body.id, { is_delete: true }, { new: true }, (err, data) => {
         if (data.is_delete) {
           res.status(200).json({
             status: true,
@@ -316,7 +313,7 @@ app.post("/delete-produto", (req, res) => {
 });
 
 /*Api to get and search product with pagination and search by name*/
-app.get("/get-produto", (req, res) => {
+app.get("/get-product", (req, res) => {
   try {
     var query = {};
     query["$and"] = [];
@@ -331,17 +328,17 @@ app.get("/get-produto", (req, res) => {
     }
     var perPage = 5;
     var page = req.query.page || 1;
-    produto.find(query, { date: 1, name: 1, id: 1, desc: 1, price: 1, discount: 1, image: 1 })
+    product.find(query, { date: 1, name: 1, id: 1, desc: 1, price: 1, discount: 1, image: 1 })
       .skip((perPage * page) - perPage).limit(perPage)
       .then((data) => {
-        produto.find(query).count()
+        product.find(query).count()
           .then((count) => {
 
             if (data && data.length > 0) {
               res.status(200).json({
                 status: true,
                 title: 'Product retrived.',
-                produtos: data,
+                products: data,
                 current_page: page,
                 total: count,
                 pages: Math.ceil(count / perPage),
@@ -369,25 +366,7 @@ app.get("/get-produto", (req, res) => {
   }
 
 });
-// importar a minha funcao de conexao com o banco
-const Conn = require('./conn/conn');
 
-// inicializar o express e atribuir em uma constante
-
-
-// executando a minha funcao de conexao com o banco criado no arquivo conn.js
-
-// buscando os dados da variavel de ambiente
-const db_url = process.env.DB_URL;
-const db_user = process.env.DB_USER;
-const db_pass = process.env.DB_PASS;
-const db_data = process.env.DB_DATA;
-Conn(db_url, db_user, db_pass, db_data);
-
-// inicializar o servidor http em alguma porta para podermos acessar ele.
-const port = 3001;
-app.listen(process.env.PORT || port, () => {
-  console.log(`O servidor esta rodando na porta ${port}`);
-})
-
-
+app.listen(2000, () => {
+  console.log("Server is Runing On port 2000");
+});
