4f96262b7e16a2798879ed448db89d76e923e34e
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-03-19 20:49:07 -0300

    j

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://cluster0.66bqn.mongodb.net/login', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
+mongoose.connect('mongodb+srv://jumf:711179@cluster0.66bqn.mongodb.net/login', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
     if (err) throw err;
     console.log('Base de datos online');
 });
