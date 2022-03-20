6269d44aedf177afb9b14a2b13e38cc96f3f8103
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-03-20 17:38:24 -0300

    sim

--- config/config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://cluster0.66bqn.mongodb.net/login', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, res) => {
-    if (err) throw err;
-    console.log('Base de datos online');
-});
-
-module.exports=mongoose
